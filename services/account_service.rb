38c5d3b34570782803f49ae6d2d5071389d8a06e
kyontan <kyontan@monora.me>
2016-03-22 02:51:26 +0900

    Implement AccountServiceHelpers

---
+++ services/account_service.rb
@@ -0,0 +1,31 @@
+require "sinatra/base"
+require_relative "../db/model"
+
+module Sinatra
+	module AccountServiceHelpers
+		def require_login(*block)
+			halt 401 unless logged_in?
+		end
+
+		def logged_in?
+			return false if session[:member_id].nil?
+			return Member.exists?(id: session[:member_id])
+		end
+
+		def current_user
+			return nil if not logged_in?
+			Member.find_by(session[:member_id])
+		end
+
+		def login_as(member_id)
+			raise if not Member.exists?(id: member_id)
+			session[:member_id] = member_id
+		end
+
+		def logout
+			session.delete(:member_id)
+		end
+	end
+
+	helpers AccountServiceHelpers
+end
