9b7bb7e30cb19608b1170aea2a8aaccaf10fe134
kyontan <kyontan@monora.me>
2016-03-22 00:53:37 +0900

    Add validation to check presence of association object, and styled code

--- db/model.rb
+++ db/model.rb
@@ -26,6 +26,7 @@ class Member < ActiveRecord::Base
   validates :name, presence: true
   validates :login, presence: true, uniqueness: true
   validates :hashed_password, presence: true
+  validates :team, presence: true, if: Proc.new {|member| not member.team_id.nil? }
 
   has_many :marked_scores   , foreign_key: "marker_id" , class_name: "Score"  , dependent: :nullify
   has_many :created_problems, foreign_key: "creator_id", class_name: "Problem", dependent: :destroy
@@ -35,20 +36,22 @@ class Member < ActiveRecord::Base
 end
 
 class Problem < ActiveRecord::Base
-  validates :title, presence: true
-  validates :text, presence: true
+  validates :title,     presence: true
+  validates :text,      presence: true
   validates :opened_at, presence: true
   validates :closed_at, presence: true
+  validates :creator,   presence: true
 
-  has_many :answers, dependent: :destroy
+  has_many :answers,  dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :issues, dependent: :destroy
+  has_many :issues,   dependent: :destroy
 
   belongs_to :creator, required: true, foreign_key: "creator_id", class_name: "Member"
 end
 
 class Issue < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title,   presence: true
+  validates :problem, presence: true
   validates :closed, inclusion: { in: [true, false] }
 
   has_many :comments, dependent: :destroy
@@ -57,7 +60,10 @@ class Issue < ActiveRecord::Base
 end
 
 class Answer < ActiveRecord::Base
-  validates :text, presence: true
+  validates :text,    presence: true
+  validates :problem, presence: true
+  validates :score,   presence: true, if: Proc.new {|answer| not answer.score_id.nil? }
+  validates :team,    presence: true, if: Proc.new {|answer| not answer.team_id.nil? }
 
   belongs_to :problem, required: true
   belongs_to :score
@@ -65,7 +71,9 @@ class Answer < ActiveRecord::Base
 end
 
 class Score < ActiveRecord::Base
-  validates :point, presence: true
+  validates :point,  presence: true
+  validates :answer, presence: true
+  validates :marker, presence: true
 
   belongs_to :answer, required: true
   belongs_to :marker, required: true, foreign_key: "marker_id", class_name: "Member"
@@ -73,7 +81,10 @@ class Score < ActiveRecord::Base
 end
 
 class Comment < ActiveRecord::Base
-  validates :text, presence: true
+  validates :text,    presence: true
+  validates :member,  presence: true
+  validates :problem, presence: true, if: Proc.new {|comment| not comment.problem_id.nil? }
+  validates :issue,   presence: true, if: Proc.new {|comment| not comment.issue_id.nil? }
   validates :required_reply, inclusion: { in: [true, false] }
 
   belongs_to :member, required: true
