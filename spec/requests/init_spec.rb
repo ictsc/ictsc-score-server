a623ec8937c294a42e4578b2f320d7a10eec2b0d
kyontan <kyontan@monora.me>
2016-03-25 02:49:54 +0900

    Add create member test / logging in test

--- spec/requests/init_spec.rb
+++ spec/requests/init_spec.rb
@@ -9,3 +9,56 @@ describe "GET /" do
     it { expect(response).to be_ok }
   end
 end
+
+describe "Member" do
+  subject(:response) { post "/api/members", params }
+
+  let (:params) do
+    {
+      admin: false,
+      name: "user",
+      login: "user",
+      password: "test"
+    }
+  end
+
+  context "Create member" do
+    it { expect(response).to be_created }
+  end
+end
+
+describe "Session" do
+  context "Login with missing credential" do
+    subject(:response) do
+      post "/api/session", params
+    end
+
+    let(:params) do
+      {
+        login: "user",
+        password: "test2"
+      }
+    end
+
+    let(:parsed_response) { p response; JSON.parse(response.body) }
+
+    it { expect(parsed_response).to eq ({ "status" => "failed"}) }
+  end
+
+  context "Login with correct credential" do
+    subject(:response) do
+      post "/api/session", params
+    end
+
+    let (:params) do
+      {
+        login: "user",
+        password: "test"
+      }
+    end
+
+    let(:parsed_response) { p response; JSON.parse(response.body) }
+
+    it { expect(parsed_response).to eq ({ "status" => "success"}) }
+  end
+end
