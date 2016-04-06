eca75863fc1a72be8776968059302c901434a9f1
kyontan <kyontan@monora.me>
2016-04-06 23:29:40 +0900

    Add /{answer|issue|problem}/:id/comments/... and abolish /api/comments

--- controllers/comment.rb
+++ controllers/comment.rb
@@ -7,69 +7,74 @@ class CommentRoutes < Sinatra::Base
   helpers Sinatra::JSONHelpers
   helpers Sinatra::AccountServiceHelpers
 
-  before "/api/comments*" do
-    I18n.locale = :en if request.xhr?
-    require_login
-  end
-
-  get "/api/comments" do
-    json Comment.all
-  end
-
-  before "/api/comments/:id" do
-    halt 404 if not Comment.exists?(id: params[:id])
-    @comment = Comment.find_by(id: params[:id])
-
-    if request.post? || request.put? || request.patch? || request.delete?
-      halt 403 if (@comment.member_id != current_user.id) and (not current_user&.admin)
+  [Answer, Issue, Problem].each do |klass|
+    pluralize_name = klass.to_s.downcase.pluralize
+    before "/api/#{pluralize_name}/:commentable_id/comments*" do
+      I18n.locale = :en if request.xhr?
+      require_login
     end
-  end
 
-  get "/api/comments/:id" do
-    json Comment.find_by(id: params[:id])
-  end
+    get "/api/#{pluralize_name}/:commentable_id/comments" do
+      json Comment.where(commentable_type: klass.to_s)
+    end
 
-  post "/api/comments" do
-    @attrs = attribute_values_of_class(Comment)
-    @attrs[:member_id] = current_user.id
-    @comment = Comment.new(@attrs)
+    before "/api/#{pluralize_name}/:commentable_id/comments/:comment_id" do
+      halt 404 if not Comment.exists?(id: params[:comment_id], commentable_type: klass.to_s)
+      @comment = Comment.find_by(id: params[:comment_id], commentable_type: klass.to_s)
 
-    if @comment.save
-      status 201
-      headers "Location" => to("/api/comments/#{@comment.id}")
+      if request.post? || request.put? || request.patch? || request.delete?
+        halt 403 if (@comment.member_id != current_user.id) and (not current_user&.admin)
+      end
+    end
+
+    get "/api/#{pluralize_name}/:commentable_id/comments/:comment_id" do
       json @comment
-    else
-      json @comment.errors
-    end
-  end
-
-  update_comment_block = Proc.new do
-    if request.put? and not satisfied_required_fields?(Comment)
-      halt 400, { required: insufficient_fields(Comment) }.to_json
     end
 
-    @attrs = attribute_values_of_class(Comment)
-    @comment.attributes = @attrs
+    post "/api/#{pluralize_name}/:commentable_id/comments" do
+      @attrs = attribute_values_of_class(Comment)
+      @attrs[:member_id] = current_user.id
+      @attrs[:commentable_type] = klass.to_s
+      @attrs[:commentable_id] = params[:commentable_id]
+      @comment = Comment.new(@attrs)
 
-    halt 400, json(@comment.errors) if not @comment.valid?
-
-    if @comment.save
-      json @comment
-    else
-      json @comment.errors
+      if @comment.save
+        status 201
+        headers "Location" => to("/api/#{pluralize_name}/:commentable_id/comments/#{@comment.id}")
+        json @comment
+      else
+        json @comment.errors
+      end
     end
-  end
 
-  put "/api/comments/:id", &update_comment_block
-  patch "/api/comments/:id", &update_comment_block
+    update_comment_block = Proc.new do
+      if request.put? and not satisfied_required_fields?(Comment)
+        halt 400, { required: insufficient_fields(Comment) }.to_json
+      end
 
-  delete "/api/comments/:id" do
-    if @comment.destroy
-      status 204
-      json status: "success"
-    else
-      status 500
-      json status: "failed"
+      @attrs = attribute_values_of_class(Comment)
+      @comment.attributes = @attrs
+
+      halt 400, json(@comment.errors) if not @comment.valid?
+
+      if @comment.save
+        json @comment
+      else
+        json @comment.errors
+      end
+    end
+
+    put "/api/#{pluralize_name}/:commentable_id/comments/:comment_id", &update_comment_block
+    patch "/api/#{pluralize_name}/:commentable_id/comments/:comment_id", &update_comment_block
+
+    delete "/api/#{pluralize_name}/:commentable_id/comments/:comment_id" do
+      if @comment.destroy
+        status 204
+        json status: "success"
+      else
+        status 500
+        json status: "failed"
+      end
     end
   end
 end
