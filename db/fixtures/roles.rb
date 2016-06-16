96e36dc55bc4d85f8cc0219dc4ab1c72eb76a9d5
kyontan <kyontan@monora.me>
2016-06-16 22:41:56 +0900

    Add nologin role

--- db/fixtures/roles.rb
+++ db/fixtures/roles.rb
@@ -1,23 +1,29 @@
 Role.seed do |r|
-  r.id = 1
+  r.id = 2
   r.name = "Admin"
   r.rank = 1
 end
 
 Role.seed do |r|
-  r.id = 2
+  r.id = 3
   r.name = "Writer"
   r.rank = 10
 end
 
 Role.seed do |r|
-  r.id = 3
+  r.id = 4
   r.name = "Participant"
   r.rank = 20
 end
 
 Role.seed do |r|
-  r.id = 4
+  r.id = 5
   r.name = "Viewer"
   r.rank = 20
+end
+
+Role.seed do |r|
+  r.id = 1
+  r.name = "Nologin"
+  r.rank = 99
 end
