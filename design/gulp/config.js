1e2ddcd64cd4294064994c87ec3acded8c06bf88
kyontan <kyontan@monora.me>
2016-07-08 03:20:25 +0900

    change directory of  installing bower components / destination of gulp build

--- design/gulp/config.js
+++ design/gulp/config.js
@@ -2,8 +2,10 @@ var path = require('path');
 var root = path.resolve(__dirname + '/..')
 
 module.exports = {
-	root: root + '/',
+	root: root + '/../',
 	src: root + '/public/assets/src/',
-	dest: root + '/public/assets/dist/',
+	dest: root + '/../public/assets/dist/',
+	// src: root + '/public/assets/src/',
+	// dest: root + '/public/assets/dist/',
 }
 
