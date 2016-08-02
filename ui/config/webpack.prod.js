ddfc4846b909fdf8cc87386ba71679800856e487
kamijin_fanta <kamijin@live.jp>
2016-08-02 15:21:38 +0900

    カスタムのイメージ使用　書き出し作変更

--- ui/config/webpack.prod.js
+++ ui/config/webpack.prod.js
@@ -59,7 +59,7 @@ module.exports = webpackMerge(commonConfig, {
      *
      * See: http://webpack.github.io/docs/configuration.html#output-path
      */
-    path: helpers.root('../dist'),
+    path: helpers.root('../public'),
 
     /**
      * Specifies the name of each output file on disk.
