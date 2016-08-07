b492ddd58f563791309aa64ae7c1dd5d435ae917
Takuya Fukuju <chalkygames123@icloud.com>
2016-08-08 04:33:01 +0900

    デザイン用ディレクトリを整理。

--- design/gulp/tasks/build.js
+++ design/gulp/tasks/build.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp')
-var runSequence = require('run-sequence')
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
 
 var build = function(done) {
 	runSequence(
-		['jade', 'sass', 'minify-image'],
+		['pug', 'sass', 'minify-image'],
 		'minify-css',
 		done
 	)
-}
+};
 
-gulp.task('build', build)
+gulp.task('build', build);
--- design/gulp/tasks/watch.js
+++ design/gulp/tasks/watch.js
@@ -1,11 +1,11 @@
-var gulp = require('gulp')
-var config = require('../config')
-var $ = require('gulp-load-plugins')()
+var gulp = require('gulp');
+var config = require('../config');
+var $ = require('gulp-load-plugins')();
 
 var watch = function() {
-	gulp.watch(config.src + 'jade/**/*.jade', ['jade'])
-	gulp.watch(config.src + 'scss/**/*.scss', ['minify-css'])
-	gulp.watch(config.src + 'images/*.{png,svg}', ['minify-image'])
+	gulp.watch(config.src + 'pug/**/*.pug', ['pug']);
+	gulp.watch(config.src + 'scss/**/*.scss', ['minify-css']);
+	gulp.watch(config.src + 'images/*.!(ai)', ['minify-image'])
 };
 
-gulp.task('watch', ['build'], watch)
+gulp.task('watch', ['build'], watch);
