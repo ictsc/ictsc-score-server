02ccc37aaaef9a78bb61f572940a7d8c450a18db
kamijin_fanta <kamijin@live.jp>
2016-08-17 16:34:27 +0900

    webpackのプロダクションの書き出し先もui/distに

--- ui/config/webpack.prod.js
+++ ui/config/webpack.prod.js
@@ -59,7 +59,7 @@ module.exports = webpackMerge(commonConfig, {
      *
      * See: http://webpack.github.io/docs/configuration.html#output-path
      */
-    path: helpers.root('../public'),
+    path: helpers.root('dist'),
 
     /**
      * Specifies the name of each output file on disk.
