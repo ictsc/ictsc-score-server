078e1fa142cae3437a2bb2b960f923e7583a13e0
kamijin_fanta <kamijin@live.jp>
2016-08-25 12:25:40 +0900

    問題一覧をちゃんと表示する

--- ui/src/app/problems/problems.component.ts
+++ ui/src/app/problems/problems.component.ts
@@ -14,10 +14,50 @@ export class Problems extends MiniList {
   }
 
   get(){
-    return this.api.problems.get();
+    return this.api.problems.get().map(problems => {
+      let res = [
+        {title: "2日目 - 午後問題", items: []},
+        {title: "2日目 - 午前問題", items: []},
+        {title: "1日目 - 午後問題", items: []},
+        {title: "1日目 - 午前問題", items: []},
+      ];
+      let push = (pos, problem) => {
+        if(!res[pos].items.find(p => p.id == problem.id))
+          res[pos].items.push(problem);
+      }
+      for(let problem of problems){
+        let opened = new Date(problem.opened_at);
+        let closed = new Date(problem.closed_at);
+        for(let date of [opened, closed]){
+          if(date.valueOf() < new Date("2016-08-27 12:30").valueOf())
+            push(3, problem);
+          else if(date.valueOf() < new Date("2016-08-27 23:59:59").valueOf())
+            push(2, problem);
+          else if(date.valueOf() < new Date("2016-08-28 12:30").valueOf())
+            push(1, problem);
+          else
+            push(0, problem);
+        }
+      }
+      return res;
+    });
   }
 
-  problem(section: any){
+  compTime(compareDate, mode){
+    let now = new Date().valueOf();
+    let date = new Date(compareDate).valueOf()
+
+    switch(mode){
+      case 1:  // close
+        return date < now + 10 * 60 * 1000 && date > now;  // 10分
+      case 2:  // publish
+        return date > now - 10 * 60 * 1000;  // 10分
+      case 3:  // closed
+        return date < now;
+    }
+  }
+
+  get problems(){
     return this.list;
   }
 
