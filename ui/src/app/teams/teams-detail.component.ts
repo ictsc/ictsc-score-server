94015651759c9a84dfcdca9f66c7aefe222c5d75
kamijin_fanta <kamijin@live.jp>
2016-08-26 17:28:06 +0900

    チーム編集

--- ui/src/app/teams/teams-detail.component.ts
+++ ui/src/app/teams/teams-detail.component.ts
@@ -1,7 +1,7 @@
 import { Response } from '@angular/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ApiService, MiniForm } from "../common";
+import { ApiService, MiniForm, Editable } from "../common";
 import { Signup } from "../login/signup.component";
 
 import { Observable } from "rxjs";
@@ -9,7 +9,7 @@ import { Observable } from "rxjs";
 @Component({
   selector: TeamsDetail.name,
   template: require('./teams-detail.template.jade'),
-  directives: []
+  directives: [ Editable ]
 })
 export class TeamsDetail extends MiniForm {
   constructor(private api: ApiService, private route: ActivatedRoute) {super();}
@@ -20,24 +20,41 @@ export class TeamsDetail extends MiniForm {
   private teamId: string;
   private teamsDetail = [];
 
+  edit = false;
+
   ngOnInit() {
     this.route.params.subscribe(parms => {
       this.teamId = parms["id"];
     });
+    this.fetch();
+  }
+
+  fetch(){
     this.api.teamsDetail().subscribe(d => {
       console.log("details", d)
       this.teamsDetail = d;
+      this.team = this.teamsDetail.find(t => t.id == this.teamId)
     });
   }
 
-  get team(){
-    return this.teamsDetail.find(t => t.id == this.teamId);
-  }
+  team;
   getPointStr(answer_id: string){
     // let point = this.getPoint(answer_id);
     // if(point) return `${point} 点`;
     // else return "未採点";
     return
+  }
+
+  update(){
+    this.api.teams.modify(this.team.id, {
+      organization: this.team.organization,
+      name: this.team.name
+    }).subscribe(r => this.cancel());
+  }
+
+  cancel(){
+    this.edit = false;
+    this.fetch();
   }
 }
 
