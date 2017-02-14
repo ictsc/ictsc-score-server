57d0534047cba28f7880d792c3e0f6d1813fa796
kyontan <kyontan@monora.me>
2017-02-14 17:22:08 +0900

    Add solved_teams_count entity for problems

--- controllers/problem.rb
+++ controllers/problem.rb
@@ -27,17 +27,48 @@ class ProblemRoutes < Sinatra::Base
       (readables + problems.where.not(id: readables.ids).select(show_columns)).sort_by(&:id)
     end
 
-    json @problems, @json_options
+    @problems_array = @problems.as_json(@json_options)
+
+    solved_teams_count_by_problem = Problem \
+      .all \
+      .joins(answers: [:score]) \
+      .group(:id, "answers.team_id") \
+      .having("SUM(scores.point) >= problems.reference_point") \
+      .select("id", "answers.team_id") \
+      .inject(Hash.new(0)){|acc, p| acc[p.id] += 1; acc }
+
+    @problems_array.each do |p|
+      p["solved_teams_count"] = solved_teams_count_by_problem[p["id"]]
+    end
+
+    json @problems_array
   end
 
   before "/api/problems/:id" do
-    @problem = Problem.includes(:comments) \
-                      .find_by(id: params[:id])
+    problems = if request.request_method == "GET"
+        Problem.includes(:comments, answers: [:score])
+      else
+        Problem.includes(:comments)
+      end
+
+    @problem = problems.find_by(id: params[:id])
+
     halt 404 if not @problem&.allowed?(by: current_user, method: request.request_method)
   end
 
   get "/api/problems/:id" do
-    json @problem, @json_options
+    solved_teams_count = Answer \
+      .joins(:score) \
+      .where(problem_id: @problem.id) \
+      .group(:team_id) \
+      .having("SUM(scores.point) >= ?", problem.reference_point) \
+      .count \
+      .count
+
+    @problem_hash = @problems.as_json(@json_options)
+    @problem_hash["solved_teams_count"] = solved_teams_count
+
+    json @problem_hash
   end
 
   post "/api/problems" do
