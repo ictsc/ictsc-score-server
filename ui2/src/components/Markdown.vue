8f8bbd9a4319ff18a27df4714b027b62b81268ea
kamijin-fanta <kamijin@live.jp>
2017-02-21 17:37:58 +0900

    add markdown components

---
+++ ui2/src/components/Markdown.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="markdown-body" v-html="markdown"></div>
+</template>
+
+<style>
+.markdown-body p {
+  white-space: pre;
+}
+</style>
+
+<script>
+import marked from 'marked';
+require('github-markdown-css/github-markdown.css');
+
+export default {
+  name: 'empty',
+  props: {
+    value: String,
+  },
+  data () {
+    return {
+      text: '',
+    }
+  },
+  asyncData: {
+  },
+  computed: {
+    markdown () {
+      return marked(this.text, { sanitize: true })
+    },
+  },
+  watch: {
+    value (val) {
+      if (val === this.text) return;
+      this.text = val;
+    },
+  },
+  mounted () {
+    this.text = this.value;
+  },
+  destroyed () {
+  },
+  methods: {
+  },
+}
+</script>
---
+++ ui2/src/components/SimpleMarkdownEditor.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="markdown-editor">
+      <textarea></textarea>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.editor-toolbar.fullscreen {
+  z-index: 1100;
+}
+</style>
+
+<script>
+require('simplemde/dist/simplemde.min.css');
+import SimpleMDE from 'simplemde';
+
+export default {
+  name: 'simple-markdown-editor',
+  props: {
+    value: String,
+    config: {
+      type: Object,
+      default () {
+        return {
+          spellChecker: false,
+          showIcons: ['code', 'horizontal-rule', 'link', 'table'],
+          guide: '/#/guide',
+        };
+      },
+    }
+  },
+  data () {
+    return {
+      simplemde: undefined,
+    }
+  },
+  asyncData: {
+  },
+  computed: {
+  },
+  watch: {
+    value (val) {
+      if (val === this.simplemde.value()) return;
+      this.simplemde.value(val);
+    },
+  },
+  mounted () {
+    this.init();
+  },
+  destroyed () {
+  },
+  methods: {
+    init () {
+      this.simplemde = new SimpleMDE(this.config);
+      this.simplemde.codemirror.on('change', () => {
+        this.$emit('input', this.simplemde.value());
+      });
+    },
+    stop () {
+      this.simplemde.toTextArea();
+      this.simplemde = undefined;
+    },
+  },
+}
+</script>
+
