16f441b2db899dee4ab245c8616c3958bb6fd2e5
kyontan <kyontan@monora.me>
2017-02-27 22:27:13 +0900

    Fix logging method

--- app.rb
+++ app.rb
@@ -26,6 +26,9 @@ require_relative "db/model"
 class App < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
+  error_logger = ::File.new("#{File.dirname(__FILE__)}/log/#{ENV["RACK_ENV"]}-error.log", "a+")
+  error_logger.sync = true
+
   use AnswerRoutes
   use AttachmentRoutes
   use CommentRoutes
@@ -49,10 +52,6 @@ class App < Sinatra::Base
   configure do
     Time.zone = "Tokyo"
     ActiveRecord::Base.default_timezone = :local
-
-    def logger
-      env['app.logger'] || env['rack.logger']
-    end
 
     # set :cometio, timeout: 120, post_interval: 2, allow_crossdomain: false
     # set :websocketio, port: 9000
@@ -82,6 +81,10 @@ class App < Sinatra::Base
     response.headers["Access-Control-Allow-Credentials"] = "true"
 
     200
+  end
+
+  before do
+    env["rack.errors"] = error_logger
   end
 
   get "/?" do
