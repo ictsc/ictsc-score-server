a116eeaf07abee872c26df90a216c44d601e5b18
kamijin-fanta <kamijin@live.jp>
2017-03-03 22:54:30 +0900

    fix guides

--- ui2/src/pages/Issues.vue
+++ ui2/src/pages/Issues.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>質問一覧</h1>
+    <div class="description">
+      <p>このページは質問一覧画面です。下のボタンを切り替えることで、表示される種類が絞り込めます。</p>
+    </div>
     <div class="tools">
       <button v-on:click="filterSelect = 0" :class="{ active: filterSelect == 0 }" class="btn label-secondary">全て</button>
       <button v-on:click="filterSelect ^= 1" :class="{ active: filterSelect & 1 }" class="btn label-danger">未回答</button>
@@ -81,6 +84,9 @@
   font-size: .9em;
 }
 
+.description {
+  text-align: center;
+}
 .tools {
   text-align: center;
   margin: 3rem 0;
