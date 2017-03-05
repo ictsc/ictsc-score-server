9d56b211b9a22aad448d19c8a8f90ca77bf7640d
kamijin-fanta <kamijin@live.jp>
2017-03-05 10:40:00 +0900

    fix problem number

--- ui2/src/components/Problem.vue
+++ ui2/src/components/Problem.vue
@@ -12,7 +12,7 @@
         <button v-else v-on:click="editEnter()" class="btn btn-secondary">編集</button>
       </div>
       <h2>
-        <span class="sub">問題2:</span>
+        <span class="sub">問題{{ problem.id }}:</span>
         <input v-if="edit" v-model="problem.title" type="text" class="form-control form-control-lg">
         <span v-else>{{ problem.title }}</span>
       </h2>
