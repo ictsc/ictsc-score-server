83af289b8e47e3365ae5850a39129a49ff3f0e90
kyontan <kyontan@monora.me>
2017-07-03 04:35:40 +0900

    Refactor specs

--- spec/requests/member_spec.rb
+++ spec/requests/member_spec.rb
@@ -1,19 +1,24 @@
 require_relative '../spec_helper.rb'
 
 describe 'members' do
+  let(:member) { build(:member, :participant) }
+  let(:admin_role) { build(:role, :admin) }
+
+  let(:params) do
+    {
+      name: member.name,
+      login: member.login,
+      password: member.password,
+      team_id: member.team_id,
+      registration_code: member.team.registration_code
+    }
+  end
+
+  let(:params_without_registration_code) { params.except(:registration_code) }
+  let(:params_with_admin_role_id) { params.merge(role_id: admin_role.id) }
+
   context 'POST /api/members', by: :nologin do
     context 'Create participant member' do
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id,
-          registration_code: member.team.registration_code
-        }
-      end
-
       let(:response) { post '/api/members', params }
 
       it { expect(response).to be_created }
@@ -23,36 +28,13 @@ describe 'members' do
     end
 
     context 'Create participant member with missing registration_code' do
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id
-        }
-      end
-
-      let(:response) { post '/api/members', params }
+      let(:response) { post '/api/members', params_without_registration_code }
 
       it { expect(response).to be_bad_request }
     end
 
     context 'Create admin member' do
-      let (:admin_role) { build(:role, :admin) }
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id,
-          registration_code: member.team.registration_code,
-          role_id: admin_role.id
-        }
-      end
-
-      let(:response) { post '/api/members', params }
+      let(:response) { post '/api/members', params_with_admin_role_id }
 
       it { expect(response).to be_forbidden }
     end
@@ -60,37 +42,13 @@ describe 'members' do
 
   context 'POST /api/members', by: :participant do
     context 'Create participant member' do
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id,
-          registration_code: member.team.registration_code
-        }
-      end
-
       let(:response) { post '/api/members', params }
 
       it { expect(response).to be_forbidden }
     end
 
     context 'Create admin member' do
-      let (:admin_role) { build(:role, :admin) }
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id,
-          registration_code: member.team.registration_code,
-          role_id: admin_role.id
-        }
-      end
-
-      let(:response) { post '/api/members', params }
+      let(:response) { post '/api/members', params_with_admin_role_id }
 
       it { expect(response).to be_forbidden }
     end
@@ -98,17 +56,6 @@ describe 'members' do
 
   context 'POST /api/members', by: :writer do
     context 'Create participant member' do
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id,
-          registration_code: member.team.registration_code
-        }
-      end
-
       let(:response) { post '/api/members', params }
 
       it { expect(response).to be_created }
@@ -118,36 +65,13 @@ describe 'members' do
     end
 
     context 'Create participant member with missing registration_code' do
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id
-        }
-      end
-
-      let(:response) { post '/api/members', params }
+      let(:response) { post '/api/members', params_without_registration_code }
 
       it { expect(response).to be_created }
     end
 
     context 'Create admin member' do
-      let (:admin_role) { build(:role, :admin) }
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id,
-          registration_code: member.team.registration_code,
-          role_id: admin_role.id
-        }
-      end
-
-      let(:response) { post '/api/members', params }
+      let(:response) { post '/api/members', params_with_admin_role_id }
 
       it { expect(response).to be_forbidden }
     end
@@ -155,17 +79,6 @@ describe 'members' do
 
   context 'POST /api/members', by: :admin do
     context 'Create participant member' do
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id,
-          registration_code: member.team.registration_code
-        }
-      end
-
       let(:response) { post '/api/members', params }
 
       it { expect(response).to be_created }
@@ -175,36 +88,13 @@ describe 'members' do
     end
 
     context 'Create participant member with missing registration_code' do
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id
-        }
-      end
-
-      let(:response) { post '/api/members', params }
+      let(:response) { post '/api/members', params_without_registration_code }
 
       it { expect(response).to be_created }
     end
 
     context 'Create admin member' do
-      let (:admin_role) { build(:role, :admin) }
-      let (:member) { build(:member, :participant) }
-      let (:params) do
-        {
-          name: member.name,
-          login: member.login,
-          password: member.password,
-          team_id: member.team_id,
-          registration_code: member.team.registration_code,
-          role_id: admin_role.id
-        }
-      end
-
-      let(:response) { post '/api/members', params }
+      let(:response) { post '/api/members', params_with_admin_role_id }
 
       it { expect(response).to be_created }
       it { expect(json_response).to include('id', 'name', 'login', 'team_id', 'created_at', 'updated_at', 'role_id') }
