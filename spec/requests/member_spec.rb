71b29d8a41c9fed24deaf3f934092b29a3a2b057
kyontan <kyontan@monora.me>
2017-07-03 03:44:55 +0900

    Add specs about creating members

--- spec/requests/member_spec.rb
+++ spec/requests/member_spec.rb
@@ -1,24 +1,225 @@
 require_relative '../spec_helper.rb'
 
-describe 'Members' do
+describe '/api/members' do
   include ApiHelpers
 
-  context 'Create participant member' do
-    let (:member) { build(:member, :participant) }
-    let (:params) do
-      {
-        name: member.name,
-        login: member.login,
-        password: member.password,
-        team_id: member.team_id,
-        registration_code: member.team.registration_code
-      }
+  context 'not_logged_in' do
+    include_context 'not_logged_in'
+
+    context 'Create participant member' do
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id,
+          registration_code: member.team.registration_code
+        }
+      end
+
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_created }
+      it { expect(json_response).to include('id', 'name', 'login', 'team_id', 'created_at', 'updated_at', 'role_id') }
+      it { expect(json_response['role_id']).to eq member.role.id }
+      it { expect(json_response).not_to include('password') }
     end
 
-    let(:response) { post '/api/members', params }
+    context 'Create participant member with missing registration_code' do
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id
+        }
+      end
 
-    it { expect(response).to be_created }
-    it { expect(json_response).to include('id', 'name', 'login', 'team_id', 'created_at', 'updated_at', 'role_id') }
-    it { expect(json_response).not_to include('password') }
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_bad_request }
+    end
+
+    context 'Create admin member' do
+      let (:admin_role) { build(:role, :admin) }
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id,
+          registration_code: member.team.registration_code,
+          role_id: admin_role.id
+        }
+      end
+
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_forbidden }
+    end
+  end
+
+  context 'as_participant' do
+    include_context 'as_participant'
+
+    context 'Create participant member' do
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id,
+          registration_code: member.team.registration_code
+        }
+      end
+
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_forbidden }
+    end
+
+    context 'Create admin member' do
+      let (:admin_role) { build(:role, :admin) }
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id,
+          registration_code: member.team.registration_code,
+          role_id: admin_role.id
+        }
+      end
+
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_forbidden }
+    end
+  end
+
+  context 'as_writer' do
+    include_context 'as_writer'
+
+    context 'Create participant member' do
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id,
+          registration_code: member.team.registration_code
+        }
+      end
+
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_created }
+      it { expect(json_response).to include('id', 'name', 'login', 'team_id', 'created_at', 'updated_at', 'role_id') }
+      it { expect(json_response['role_id']).to eq member.role.id }
+      it { expect(json_response).not_to include('password') }
+    end
+
+    context 'Create participant member with missing registration_code' do
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id
+        }
+      end
+
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_created }
+    end
+
+    context 'Create admin member' do
+      let (:admin_role) { build(:role, :admin) }
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id,
+          registration_code: member.team.registration_code,
+          role_id: admin_role.id
+        }
+      end
+
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_forbidden }
+    end
+  end
+
+  context 'as_admin' do
+    include_context 'as_admin'
+
+    context 'Create participant member' do
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id,
+          registration_code: member.team.registration_code
+        }
+      end
+
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_created }
+      it { expect(json_response).to include('id', 'name', 'login', 'team_id', 'created_at', 'updated_at', 'role_id') }
+      it { expect(json_response['role_id']).to eq member.role.id }
+      it { expect(json_response).not_to include('password') }
+    end
+
+    context 'Create participant member with missing registration_code' do
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id
+        }
+      end
+
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_created }
+    end
+
+    context 'Create admin member' do
+      let (:admin_role) { build(:role, :admin) }
+      let (:member) { build(:member, :participant) }
+      let (:params) do
+        {
+          name: member.name,
+          login: member.login,
+          password: member.password,
+          team_id: member.team_id,
+          registration_code: member.team.registration_code,
+          role_id: admin_role.id
+        }
+      end
+
+      let(:response) { post '/api/members', params }
+
+      it { expect(response).to be_created }
+      it { expect(json_response).to include('id', 'name', 'login', 'team_id', 'created_at', 'updated_at', 'role_id') }
+      it { expect(json_response['role_id']).to eq admin_role.id }
+      it { expect(json_response).not_to include('password') }
+    end
   end
 end
--- spec/support/api_helpers.rb
+++ spec/support/api_helpers.rb
@@ -5,9 +5,39 @@ module ApiHelpers
     JSON.parse(response.body)
   end
 
+  def session
+    begin
+      { 'rack.session' => last_request.env['rack.session'] }
+    rescue Rack::Test::Error # raises if no request before
+      {}
+    end
+  end
+
+  # override methods in Rack::Test::Session to pass session
+  %i(get post put patch delete options head).each do |method|
+    define_method(method) do |uri, params = {}, env = {}, &block|
+      env.merge! session
+      super(uri, params, env, &block)
+    end
+  end
+
+  %w(admin writer participant viewer).each do |role|
+    RSpec.shared_context "as_#{role}" do
+      let!(:current_member) { create(:member, role.to_sym) }
+
+      before do
+        post '/api/session', { login: current_member.login, password: current_member.password }
+      end
+    end
+  end
+
+  RSpec.shared_context "not_logged_in" do
+    let!(:current_member) { nil }
+  end
+
   RSpec.shared_examples 'not logged in' do
     it 'returns unauthorized' do
-      expect(response.status).to eq 401
+      expect(response).to be_unauthorized
     end
   end
 end
