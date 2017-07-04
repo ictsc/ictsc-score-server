b5f144f1d080cece598a1752414b7b0d47446274
kyontan <kyontan@monora.me>
2017-07-05 04:39:36 +0900

    Add contest specs

---
+++ spec/requests/contest_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../spec_helper.rb'
+
+describe 'Contest' do
+  include ApiHelpers
+
+  describe 'GET /api/contest' do
+    let(:response) { get '/api/contest' }
+    subject { response.status }
+
+    it { is_expected.to eq 200 }
+
+    describe '#keys' do
+      let(:response) { get '/api/contest' }
+      subject { json_response.keys }
+      let(:expected_keys) {
+        %w(
+          answer_reply_delay_sec
+          first_blood_bonus_percentage
+          competition_start_at
+          scoreboard_hide_at
+          competition_end_at
+        )
+      }
+
+      it { is_expected.to match_array(expected_keys) }
+    end
+  end
+end
