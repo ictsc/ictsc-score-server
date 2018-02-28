4a94e6fbb49c5bfefd001c6044cad8263b6c472b
uplus <uplus.e10@gmail.com>
2018-02-28 19:40:45 +0900

    fix API client

--- client/client.rb
+++ client/client.rb
@@ -207,8 +207,8 @@ def register_problems_to_group(group_id:, problem_ids: [])
   end
 end
 
-host = ARGV[0] || 'localhost'
-$base_url = "http://#{host}:3000/api"
+$host = ARGV[0] || 'localhost'
+$base_url = "http://#{$host}:3000/api"
 $responses = []
 
 require 'pry'
