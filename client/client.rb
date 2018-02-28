cdd34c21b3faec63799ca528c2682992dae119f5
uplus <uplus.e10@gmail.com>
2018-02-28 19:03:23 +0900

    メソッドの並びソートした

--- client/client.rb
+++ client/client.rb
@@ -6,6 +6,8 @@ host = ARGV[0] || 'localhost'
 $base_url = "http://#{host}:3000/api"
 $responses = []
 
+## utils
+
 def build_url(path)
   File.join($base_url, path)
 end
@@ -18,12 +20,29 @@ def request(method, path, payload_hash = {}, headers = { content_type: :json })
   $responses.last
 end
 
+def parse_file(filepath)
+  filepath = File.expand_path(filepath)
+  case File.extname(filepath)
+  when '.yml', '.yaml'
+    YAML.load(File.read(filepath))
+  when '.json'
+    JSON.parse(File.read(filepath))
+  end
+end
+
+
 def login_as(login:, password:)
   JSON.parse(request(:post, 'session', { login: login, password: password }))
 end
 
 def logout
   JSON.parse(request(:delete, 'session'))
+end
+
+## problem groups
+
+def list_problem_groups()
+  JSON.parse(request(:get, 'problem_groups'))
 end
 
 def add_problem_group(name:, description:, visible: true, completing_bonus_point: 0, flag_icon_url: '')
@@ -36,6 +55,25 @@ def add_problem_group(name:, description:, visible: true, completing_bonus_point
   }
 
   request(:post, 'problem_groups', data)
+end
+
+def add_problem_groups_from_hash(problem_groups)
+  problem_groups.each do |g|
+    puts add_problem_group(
+      name: g['name'],
+      description: g['description'],
+      visible: g['visible'],
+      completing_bonus_point: g['completing_bonus_point'],
+      flag_icon_url: g['flag_icon_url'],
+    )
+  end
+end
+
+## problems
+
+def list_problems(with: [])
+  with_params = with.empty? ? '' : "?with=#{with.join(',')}"
+  JSON.parse(request(:get, 'problems' + with_params))
 end
 
 def add_problem(title:, text:, reference_point:, perfect_point:, creator_id:, problem_group_ids:, problem_must_solve_before_id:)
@@ -52,9 +90,29 @@ def add_problem(title:, text:, reference_point:, perfect_point:, creator_id:, pr
   JSON.parse(request(:post, 'problems', data))
 end
 
+def add_problems_from_hash(problems)
+  problems.each do |p|
+    puts add_problem(
+      title: p['title'],
+      text: p['text'],
+      reference_point: p['reference_point'],
+      perfect_point: p['perfect_point'],
+      creator_id: p['creator_id'],
+      problem_must_solve_before_id: p['problem_must_solve_before_id'],
+      problem_group_ids: p['problem_group_ids'],
+    )
+  end
+end
+
 # def update_problem(id:, title:, text:, reference_point:, perfect_point:, creator_id:, problem_group_ids:, problem_must_solve_before_id:)
 def update_problem(problem_hash)
   JSON.parse(request(:put, "problems/#{problem_hash['id']}", problem_hash))
+end
+
+## teams
+
+def list_teams
+  JSON.parse(request(:get, 'teams'))
 end
 
 def add_team(name:, organization:, registration_code:)
@@ -76,18 +134,7 @@ def add_teams_from_hash(teams)
   end
 end
 
-def list_members()
-  JSON.parse(request(:get, 'members'))
-end
-
-def list_problems(with: [])
-  with_params = with.empty? ? '' : "?with=#{with.join(',')}"
-  JSON.parse(request(:get, 'problems' + with_params))
-end
-
-def list_problem_groups()
-  JSON.parse(request(:get, 'problem_groups'))
-end
+## attachments
 
 def add_attachments(filepath)
   full_filepath = File.expand_path(filepath)
@@ -104,6 +151,11 @@ def download_attachments(id:, file_hash:, file_name:)
   JSON.parse(request(:get, path))
 end
 
+## members
+
+def list_members()
+  JSON.parse(request(:get, 'members'))
+end
 
 # role_id: 2=admin, 3=writer 4=participant 5=viewer
 # writer,admin,viewerは team_idとregistration_codeをnullにしてrole_idを指定する
@@ -133,50 +185,13 @@ def add_members_from_hash(members)
   end
 end
 
-# まとめて流し込み系
-# YAML,JSONを読み込む
-def parse_file(filepath)
-  filepath = File.expand_path(filepath)
-  case File.extname(filepath)
-  when '.yml', '.yaml'
-    YAML.load(File.read(filepath))
-  when '.json'
-    JSON.parse(File.read(filepath))
-  end
-end
 
-def add_problems_from_hash(problems)
-  problems.each do |p|
-    puts add_problem(
-      title: p['title'],
-      text: p['text'],
-      reference_point: p['reference_point'],
-      perfect_point: p['perfect_point'],
-      creator_id: p['creator_id'],
-      problem_must_solve_before_id: p['problem_must_solve_before_id'],
-      problem_group_ids: p['problem_group_ids'],
-    )
-  end
-end
-
-def add_problem_groups_from_hash(problem_groups)
-  problem_groups.each do |g|
-    puts add_problem_group(
-      name: g['name'],
-      description: g['description'],
-      visible: g['visible'],
-      completing_bonus_point: g['completing_bonus_point'],
-      flag_icon_url: g['flag_icon_url'],
-    )
-  end
-end
+#### 操作サンプル(雑) ####
 
 # TODO: valid_probleとか作るproblemからproblem_groupを読み取って自動で作る
 
 
-#### 操作サンプル(雑) ####
-
-puts r_login = login_as(login: 'admin', password: 'admin')
+# puts r_login = login_as(login: 'admin', password: 'admin')
 # puts r_login = login_as(login: 'f_1', password: 'f_1')
 
 # puts add_attachments('./pry_r.rb')
