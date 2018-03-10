35254bcec8df5ab50056351c5ae852b401cde81c
uplus <uplus.e10@gmail.com>
2018-03-10 19:25:18 +0900

    ui対応

--- ui2/src/utils/Utils.js
+++ ui2/src/utils/Utils.js
@@ -28,3 +28,9 @@ export function minBy (arr, key) {
 
   return arr.reduce((p, n) => (Date.parse(p[key]) > Date.parse(n[key]) ? n : p));
 }
+
+// 昇順
+export function sortBy (arr, key, reverse = false) {
+  const compare = !reverse ? (p1, p2) => (p1[key] - p2[key]) : (p1, p2) => (p2[key] - p1[key]);
+  return arr.sort(compare);
+}
