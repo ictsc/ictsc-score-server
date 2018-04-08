ef568d3d8a28dcc63ec665a5928d491417d8c2c3
uplus <uplus.e10@gmail.com>
2018-04-08 21:28:19 +0900

    client: フック処理のエラー処理をリファクタリング

--- client/client.rb
+++ client/client.rb
@@ -117,16 +117,11 @@ end
 
 # フック処理失敗
 class HookError < StandardError
-  # フック内では自身のフック名が分からない
   attr_accessor :hook
 
-  def initialize(error_message)
-    @error_message = error_message
-    super('')
-  end
-
-  def to_s
-    "#{hook}: #{@error_message}"
+  def initialize(error_message, hook:)
+    @hook = hook
+    super("#{hook}: #{error_message}")
   end
 end
 
@@ -137,11 +132,11 @@ class HookRelatedRecordNotFound < HookError
 
   # endpoint: 探索先エンドポイントのシンボル
   # key: 探索キー(find_byの引数)
-  def initialize(key:, endpoint:)
+  def initialize(hook:, key:, endpoint:)
     @endpoint = endpoint.to_sym
     @key = key
 
-    super("#{@key} not found in #{@endpoint}")
+    super("#{@key} not found in #{@endpoint}", hook: hook)
   end
 end
 
@@ -157,10 +152,11 @@ end
 # 投稿処理を終了する
 class HookFileNotFound < HookError
   attr_reader :filepath
-  def initialize(filepath:)
+
+  def initialize(hook:, filepath:)
     @filepath = filepath
 
-    super("#{filepath} not found")
+    super("#{filepath} not found", hook: hook)
   end
 end
 
@@ -169,9 +165,9 @@ end
 class HookNestedRequestWarning < HookError
   attr_reader :result
 
-  def initialize(result)
+  def initialize(hook:, result:)
     @result = result
-    super(result)
+    super(result, hook: hook)
   end
 end
 
@@ -364,7 +360,7 @@ module Hooks
     value = value.to_sym.downcase
     role_id = get_roles[value]
     # 明示的にRoleを指定しようとして失敗したならエラーにする
-    raise HookRelatedRecordNotFoundError.new(key: { role_key: value }, endpoint: :roles) if role_id.nil?
+    raise HookRelatedRecordNotFoundError.new(hook: key, key: { role_key: value }, endpoint: :roles) if role_id.nil?
     this[:role_id] = role_id
   end
 
@@ -376,7 +372,7 @@ module Hooks
   # creator_idをloginで指定できる
   def member_id_by_login(key:, value:, this:, list:, index:)
     member = get_members.find_by(login: value)
-    raise HookRelatedRecordNotFoundError.new(key: { login: value }, endpoint: :members) if member.nil?
+    raise HookRelatedRecordNotFoundError.new(hook: key, key: { login: value }, endpoint: :members) if member.nil?
 
     # _creator -> creator_id
     actual_key = (key.to_s.delete_prefix('_') + '_id').to_sym
@@ -386,7 +382,7 @@ module Hooks
   # titleから依存問題を求める
   def problem_dependency_problem_by_title(key:, value:, this:, list:, index:)
     problem = get_problems.find_by(title: value)
-    raise HookRelatedRecordNotFoundWarning.new(key: { title: value }, endpoint: :problems) if problem.nil?
+    raise HookRelatedRecordNotFoundWarning.new(hook: key, key: { title: value }, endpoint: :problems) if problem.nil?
     this[:problem_must_solve_before_id] = problem[:id]
   end
 
@@ -403,7 +399,7 @@ module Hooks
 
     dependency_problem_id = list[index - 1][:id]
 
-    raise HookRelatedRecordNotFoundWarning.new(key: { list_index: index - 1 }, endpoint: :problems) if dependency_problem_id.nil?
+    raise HookRelatedRecordNotFoundWarning.new(hook: key, key: { list_index: index - 1 }, endpoint: :problems) if dependency_problem_id.nil?
 
     this[:problem_must_solve_before_id] = dependency_problem_id
   end
@@ -411,12 +407,12 @@ module Hooks
   # 問題グループ投稿時に、問題も投稿する(problem_group_idsが自動で付与される)
   def problem_group_problems(key:, value:, this:, list:, index:)
     problem_group_id = this[:id]
-    raise HookRelatedRecordNotFoundWarning.new(key: 'this', endpoint: :problem_group) if problem_group_id.nil?
+    raise HookRelatedRecordNotFoundWarning.new(hook: key, key: 'this', endpoint: :problem_group) if problem_group_id.nil?
     value.update(problem_group_ids: [problem_group_id])
     results = post_problems(value)
 
     warnings = results.select {|e| e.has_key?(:error) || e.has_key?(:warnings) }
-    raise HookNestedRequestWarning.new(result: warnings) unless warnings.empty?
+    raise HookNestedRequestWarning.new(hook: key, result: warnings) unless warnings.empty?
   end
 
   # 一括投稿時にorderを省略すると並び順になる
@@ -431,7 +427,7 @@ module Hooks
   def text_by_filepath(key:, value:, this:, list:, index:)
     text = load_file(value)
 
-    raise HookFileNotFound.new(filepath: value) if text.nil?
+    raise HookFileNotFound.new(hook: key, filepath: value) if text.nil?
 
     # _text -> text
     actual_key = key.to_s.delete_prefix('_').to_sym
@@ -442,7 +438,7 @@ module Hooks
   def attachment_file_by_filepath(key:, value:, this:, list:, index:)
     abs_filepath = File.expand_path(value)
 
-    raise HookFileNotFound.new(filepath: value) unless File.file?(abs_filepath)
+    raise HookFileNotFound.new(hook: key, filepath: value) unless File.file?(abs_filepath)
 
     this[:file] = File.open(abs_filepath, 'rb')
   end
@@ -601,17 +597,9 @@ module EndpointRequests
         .method(method_sym)
         .call(key: key, value: this[key], this: this, list: list, index: index)
 
-    rescue HookError => e
-      e.hook = key
-
-      case e
-      when HookRelatedRecordNotFoundWarning, HookNestedRequestWarning
-        # 次のフックに移る
-        warnings << e
-      else
-        # フック処理を終了する
-        raise e
-      end
+    rescue HookRelatedRecordNotFoundWarning, HookNestedRequestWarning => e
+      # フック処理を継続
+      warnings << e
     end
 
     warnings
