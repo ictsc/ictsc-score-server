f189eddc44cf681ec455f356e5a07794d37be132
uplus <uplus.e10@gmail.com>
2018-11-21 14:35:49 +0900

    update

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'factory_bot'
-
 FactoryBot.find_definitions
 
 Role.delete_all
