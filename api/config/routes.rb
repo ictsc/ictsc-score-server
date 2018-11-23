d36fd9011f152eee8bb4ec7fa7971d7033eb863d
uplus <uplus.e10@gmail.com>
2018-11-23 19:11:26 +0900

    update

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :answers
+  resources :application
+  resources :attachments
+  resources :comments
+  resources :contests
+  resources :issues
+  resources :members
+  resources :notices
+  resources :notifications
+  resources :problem_groups
+  resources :problems
+  resources :scoreboards
+  resources :scores
+  resources :teams
 end
