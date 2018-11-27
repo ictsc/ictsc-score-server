12ea422211eb4c3700053d0c9df0220b5b3b4882
uplus <uplus.e10@gmail.com>
2018-11-27 12:30:30 +0900

    とりあえず curl叩けた

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327134518) do
+ActiveRecord::Schema.define(version: 2018_03_27_134518) do
 
-  create_table "answers", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "answers", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "problem_id", null: false
     t.integer "team_id", null: false
     t.datetime "created_at", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["team_id"], name: "index_answers_on_team_id"
   end
 
-  create_table "attachments", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "attachments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "filename", null: false
     t.integer "member_id"
     t.datetime "created_at", null: false
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["member_id"], name: "index_attachments_on_member_id"
   end
 
-  create_table "comments", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "comments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "text", limit: 4000, null: false
     t.integer "member_id", null: false
     t.datetime "created_at", null: false
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
-  create_table "first_correct_answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "first_correct_answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "team_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["team_id"], name: "index_first_correct_answers_on_team_id"
   end
 
-  create_table "issues", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "issues", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.boolean "closed", default: false, null: false
     t.integer "problem_id", null: false
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["id"], name: "index_issues_on_id", unique: true
   end
 
-  create_table "members", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "members", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "login", null: false
     t.string "hashed_password", null: false
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["login"], name: "index_members_on_login", unique: true
   end
 
-  create_table "notices", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "notices", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.string "text", limit: 4000, null: false
     t.boolean "pinned", default: false, null: false
@@ -88,7 +88,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["member_id"], name: "index_notices_on_member_id"
   end
 
-  create_table "notification_subscribers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "notification_subscribers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "channel_id", null: false
     t.string "subscribable_type"
     t.bigint "subscribable_id"
@@ -97,7 +97,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["subscribable_type", "subscribable_id"], name: "index_notification_subscribers_on_subscribable", unique: true
   end
 
-  create_table "problem_groups", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "problem_groups", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
     t.datetime "created_at", null: false
@@ -109,7 +109,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["id"], name: "index_problem_groups_on_id", unique: true
   end
 
-  create_table "problem_groups_problems", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "problem_groups_problems", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "problem_id", null: false
     t.bigint "problem_group_id", null: false
     t.datetime "created_at", null: false
@@ -119,7 +119,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["problem_id"], name: "index_problem_groups_problems_on_problem_id"
   end
 
-  create_table "problems", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "problems", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.string "text", limit: 4000, null: false
     t.integer "creator_id", null: false
@@ -135,7 +135,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["problem_must_solve_before_id"], name: "index_problems_on_problem_must_solve_before_id"
   end
 
-  create_table "roles", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "roles", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.integer "rank", null: false
     t.datetime "created_at", null: false
@@ -143,7 +143,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["name"], name: "index_roles_on_name", unique: true
   end
 
-  create_table "scores", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "scores", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "point", null: false
     t.integer "answer_id", null: false
     t.integer "marker_id", null: false
@@ -154,7 +154,7 @@ ActiveRecord::Schema.define(version: 20180327134518) do
     t.index ["id"], name: "index_scores_on_id", unique: true
   end
 
-  create_table "teams", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "teams", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "organization"
     t.datetime "created_at", null: false
