a9ddc41b1754806ca4c89213de8790cfbe86ed04
uplus <uplus.e10@gmail.com>
2019-02-04 00:08:41 +0900

    extract rank! method

--- api/app/models/scoreboard.rb
+++ api/app/models/scoreboard.rb
@@ -3,23 +3,10 @@ class Scoreboard
 
   def initialize(user:)
     # [{1st_team, score}, {2nd_team, score}, {3rd_team, score}, ...]
-    @teams_scores = aggregate_each_team_score(user)
-
-    return if teams_scores.blank?
+    @teams_scores = collect_teams_scores(user)
 
     # [{1st_team, score, rank}, {2nd_team, score, rank}, {3rd_team, score, rank}, ...]
-    # 順位を付ける
-    # 同点時の順位は 1 2 2 4
-    current_rank = 1
-    previous_score = teams_scores.first.fetch(:score)
-    teams_scores.each.with_index(1) do |data, index|
-      if previous_score != data[:score]
-        previous_score = data[:score]
-        current_rank = index
-      end
-
-      data[:rank] = current_rank
-    end
+    rank!
   end
 
   def find_by_team(team)
@@ -36,20 +23,34 @@ class Scoreboard
   attr_reader :teams_scores
 
   # { [team, problem_id] => [score, ...], ...}
-  def collect_scores_of_team_and_problem(user)
+  def collect_teams_scores(user)
     Score
       .readables(user: user, action: 'scoreboard')
       .preload(:answer, :team)
       .joins(:answer)
       .select('answers.problem_id', 'answers.team_id', :point, 'answers.created_at', :answer_id)
-      .group_by {|e| [e.team, e.problem_id] }
+      .group_by(&:team)
+      .map {|team, team_scores| { team: team, score: aggregate_team_score(team_scores) } }
   end
 
-  def aggregate_each_team_score(user)
-    collect_scores_of_team_and_problem(user)
-      .transform_values {|scores| scores.max_by {|score| score.answer.created_at } }
-      .each_with_object(Hash.new(0)) {|(k, score), memo| memo[k[0]] += score.point }
-      .sort_by {|e| -e[1] }
-      .map {|e| { team: e[0], score: e[1] } }
+  # 各問題の最新の解答のスコアを集計する
+  def aggregate_team_score(team_scores)
+    team_scores
+      .group_by(&:problem_id)
+      .lazy
+      .map {|problem_id, scores| scores.max_by {|s| s.answer.created_at } }
+      .sum(&:point)
+  end
+
+  def rank!
+    return if teams_scores.empty?
+
+    teams_scores.sort_by! {|e| -e[:score] }
+    teams_scores.first[:rank] = 1
+
+    # 同点時の順位は 1 2 2 4
+    teams_scores.each_cons(2).with_index(2) do |(previous_data, data), index|
+      data[:rank] = (previous_data[:score] == data[:score]) ? previous_data[:rank] : index
+    end
   end
 end
