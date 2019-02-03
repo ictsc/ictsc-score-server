d92e639f73a59ae33d178081a3cc9ad767b397ea
uplus <uplus.e10@gmail.com>
2019-02-04 00:13:04 +0900

    move scoreboard aggregate methods to model

--- api/app/models/scoreboard.rb
+++ api/app/models/scoreboard.rb
@@ -53,4 +53,52 @@ class Scoreboard
       data[:rank] = (previous_data[:score] == data[:score]) ? previous_data[:rank] : index
     end
   end
+
+  class << self
+    def aggregate(current_user:)
+      # [{1st_team, score, rank}, {2nd_team, score, rank}, {3rd_team, score, rank}, ...]
+      scores = Scoreboard.new(user: current_user)
+
+      # when team has nothing score, this value is nil
+      my_team_rank = scores.find_by_team(current_user.team)&.fetch(:rank) unless current_user.staff?
+
+      scores.map do |score|
+        display_mode = select_display_mode(current_user, scores, score, my_team_rank)
+        next unless display_mode
+
+        build_score_info(score, display_mode)
+      end
+        .compact
+    end
+
+    private
+
+    def select_display_mode(current_user, scores, score, my_team_rank)
+      if current_user.staff? || score[:team] == current_user.team
+        :all
+      elsif score[:rank] <= Setting.scoreboard_viewable_top
+        :top
+      elsif (score[:rank] + scores.count_same_rank(score[:rank])) == my_team_rank
+        # 1ランク上のチーム全て
+        :up
+      else
+        # 表示しない
+        nil
+      end
+    end
+
+    def build_score_info(score, display_mode)
+      score_info = { rank: score[:rank] }
+
+      if Setting.scoreboard_viewable_config[display_mode][:team]
+        score_info[:team] = score[:team].as_json(only: %i[id name organization])
+      end
+
+      if Setting.scoreboard_viewable_config[display_mode][:score]
+        score_info[:score] = score[:score]
+      end
+
+      score_info
+    end
+  end
 end
