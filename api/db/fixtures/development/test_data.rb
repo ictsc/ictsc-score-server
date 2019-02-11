a3bf2d2649866d12c9bc45aa3090ca49885f1164
uplus <uplus.e10@gmail.com>
2019-02-11 15:26:43 +0900

    add fixture

--- api/db/fixtures/development/test_data.rb
+++ api/db/fixtures/development/test_data.rb
@@ -10,6 +10,22 @@ alnum    = ->(i) { [*1..i].map { ([*'a'..'z'] + [*'A'..'Z'] + [*'0'..'9']).sampl
 hiragana = ->(i) { [*1..i].map { [*'あ'..'ん'].sample }.join }
 
 # rubocop:disable Layout/ExtraSpacing, Layout/IndentArray
+Config.seed(:key, [
+  { key: :competition_time_day1_start_at, value_type: :date,    value: DateTime.parse('2012-09-03 10:00:00 +0900') },
+  { key: :competition_time_day1_end_at,   value_type: :date,    value: DateTime.parse('2112-09-03 18:00:00 +0900') },
+  { key: :competition_time_day2_start_at, value_type: :date,    value: DateTime.parse('2112-09-03 10:00:00 +0900') },
+  { key: :competition_time_day2_end_at,   value_type: :date,    value: DateTime.parse('2112-09-03 18:00:00 +0900') },
+  { key: :competition_stop,               value_type: :boolean, value: false },
+  { key: :problem_open_all_at,            value_type: :date,    value: DateTime.parse('2112-09-03 11:00:00 +0900') },
+  { key: :grading_delay_sec,              value_type: :integer, value: 30 },
+  { key: :scoreboard_hide_at,             value_type: :date,    value: DateTime.parse('2112-09-03 12:00:00 +0900') },
+  { key: :scoreboard_top,                 value_type: :integer, value: 3 },
+  { key: :scoreboard_display_top_team,    value_type: :boolean, value: true },
+  { key: :scoreboard_display_top_score,   value_type: :boolean, value: true },
+  { key: :scoreboard_display_above_team,  value_type: :boolean, value: false },
+  { key: :scoreboard_display_above_score, value_type: :boolean, value: true },
+])
+
 Team.seed(:id, [
   { id: 1,  name: 'Team 1',        organization: 'A学校',              hashed_registration_code: hash_password('teama') },
   { id: 2,  name: 'Team 2',        organization: 'B学校',              hashed_registration_code: hash_password('teamb') },
