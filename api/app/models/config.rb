f966834a23394a67c0cba4f10911cbc3aff7cc15
uplus <uplus.e10@gmail.com>
2019-02-16 01:17:03 +0900

    add access controll method to config model

--- api/app/models/config.rb
+++ api/app/models/config.rb
@@ -27,6 +27,63 @@ class Config < ApplicationRecord
     errors.add(:value_type, 'disallow to update value_type(%s to %s)' % [value_type, value_type_was]) if will_save_change_to_value_type?
   end
 
+  # method: POST
+  def self.allowed_to_create_by?(user = nil, action: '')
+    case user&.role_id
+    when ROLE_ID[:admin]
+      true
+    else
+      false
+    end
+  end
+
+  def readable?(by: nil, action: '')
+    self.class.readables(user: by, action: action).exists?(id: id)
+  end
+
+  # method: GET, PUT, PATCH, DELETE
+  def allowed?(method:, by: nil, action: '')
+    case by&.role_id
+    when ROLE_ID[:admin]
+      true
+    else
+      false
+    end
+  end
+
+  def self.allowed_nested_params(user:)
+    []
+  end
+
+  def self.readable_columns(user:, action: '', reference_keys: true)
+    case user&.role_id
+    when ROLE_ID[:admin]
+      all_column_names(reference_keys: reference_keys)
+    else
+      []
+    end
+  end
+
+  def self.filter_columns(user:, action: '')
+    cols = readable_columns(user: user, action: action, reference_keys: false)
+    cols.empty? ? none : select(*cols)
+  end
+
+  def self.readable_records(user:, action: '')
+    case user&.role_id
+    when ROLE_ID[:admin]
+      all
+    else
+      none
+    end
+  end
+
+  # method: GET
+  def self.readables(user:, action: '')
+    readable_records(user: user, action: action)
+      .filter_columns(user: user, action: action)
+  end
+
   class << self
     def get(key)
       cast(find_by(key: key))
