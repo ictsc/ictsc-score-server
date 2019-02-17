b3be0546d8250c7811933e335eb2bbe5e544ab26
NozomiSugiyama <rioc.sugiyama@gmail.com>
2019-02-17 16:42:48 +0900

    fix(ui): Remove eslint-friendly-formatter from eslint-loader options

--- ui/build/webpack.base.conf.js
+++ ui/build/webpack.base.conf.js
@@ -35,10 +35,7 @@ module.exports = {
         test: /\.(js|vue)$/,
         loader: 'eslint-loader',
         enforce: 'pre',
-        include: [resolve('src'), resolve('test')],
-        options: {
-          formatter: require('eslint-friendly-formatter')
-        }
+        include: [resolve('src'), resolve('test')]
       },
       {
         test: /\.vue$/,
