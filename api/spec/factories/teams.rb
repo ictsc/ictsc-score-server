f49d28a0e985e919e23c1d77a7e1623b2d714432
uplus <uplus.e10@gmail.com>
2019-03-03 09:23:43 +0900

    fix factory

--- api/spec/factories/teams.rb
+++ api/spec/factories/teams.rb
@@ -5,6 +5,5 @@ FactoryBot.define do
     sequence(:name) {|n| "team_#{n}" }
     organization { "organization_of_#{name}" }
     registration_code { "#{name}_registration_code" }
-    hashed_registration_code { Digest::SHA1.hexdigest("#{name}_registration_code") }
   end
 end
