01ec8fa817bd5f1d6d5da5abf8cf4b3e0e242bbe
uplus <uplus.e10@gmail.com>
2019-06-22 21:59:01 +0900

    enable simplecov

--- api2/spec/rails_helper.rb
+++ api2/spec/rails_helper.rb
@@ -9,6 +9,11 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+if ENV['CIRCLECI'] || ENV['SIMPLECOV']
+  require 'simplecov'
+  SimpleCov.start('rails')
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
