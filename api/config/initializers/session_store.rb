224a6c73aa51733f00423090c97d07ec68825fe0
uplus <uplus.e10@gmail.com>
2019-07-15 01:32:38 +0900

    improve redis config

--- api/config/initializers/session_store.rb
+++ api/config/initializers/session_store.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
+redis_url = URI.parse(ENV.fetch('REDIS_URL'))
+server = {
+  host: redis_url.host,
+  port: redis_url.port,
+  db: redis_url.path.sub(/^\//, ''),
+  namespace: 'sessions'
+}
+
 Rails.application.config.session_store(:redis_store,
-                                       servers: [ENV.fetch('REDIS_URL')],
-                                       # expire_after: 90.minutes,
+                                       servers: [server],
+                                       expire_after: ENV.fetch('API_SESSION_EXPIRE_MINUTES').to_i.minutes,
                                        key: '_session',
-                                       threadsafe: false)
+                                       threadsafe: true)
