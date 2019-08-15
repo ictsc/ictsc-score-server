0e92c16a1de008e823df71c4b587e3ffd5292592
TakeruHayasaka <t-hayasaka@sakura.ad.jp>
2019-08-16 00:41:32 +0900

    fix inventry.sh

--- k8s/inventry.sh
+++ k8s/inventry.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+set -eu
+
 if [ -e ./terraform.tfstate ]; then
-    ip=`cat terraform.tfstate | jq -r '.outputs[].value' | sed 's/\$/,/'`
-    cat << EOS
+    rm -f hosts
+    if [ "`uname`" == "Darwin" ]; then
+        ip=`cat terraform.tfstate | jq -r '.outputs[].value' | sed -e 's/\$/,/' | sed -e :loop -e 'N; $!b loop' -e 's/\n/ /g'| sed -e 's/,$//g'`
+    else
+        # linux style sed
+        ip=`cat terraform.tfstate | jq -r '.outputs[].value' | sed -e 's/\$/,/' | sed -e ':loop; N; $!b loop; s/\n/ /g'| sed -e 's/,$//g'`
+    fi
+    cat << EOS > hosts
 {
-    "cloud_servers"  : [ $ip ]
+    "cloud_servers"  : [$ip]
 }
 EOS
+    chmod +x hosts
 fi
