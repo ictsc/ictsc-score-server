0c4845994c64a6b9c1f61116a0a1e2e13348c6d4
uplus <uplus.e10@gmail.com>
2019-08-21 04:10:05 +0900

    implemented guide page

--- ui/pages/guide.vue
+++ ui/pages/guide.vue
@@ -4,18 +4,25 @@
       <v-flex>
         <v-layout column align-center>
           <page-title title="ガイド" />
+          <markdown :content="guidePage" />
         </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 <script>
+import { mapGetters } from 'vuex'
+import Markdown from '~/components/atoms/Markdown'
 import PageTitle from '~/components/atoms/PageTitle'
 
 export default {
   name: 'Guide',
   components: {
+    Markdown,
     PageTitle
+  },
+  computed: {
+    ...mapGetters('contestInfo', ['guidePage'])
   }
 }
 </script>
