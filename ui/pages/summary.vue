a77015ff05730b1c7e31d60769ba388c108b590e
uplus <uplus.e10@gmail.com>
2019-08-21 04:10:37 +0900

    add empty Summary page

---
+++ ui/pages/summary.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-layout column align-center>
+      <page-title title="状況" />
+    </v-layout>
+  </v-container>
+</template>
+<script>
+import PageTitle from '~/components/atoms/PageTitle'
+
+export default {
+  name: 'Summary',
+  components: {
+    PageTitle
+  }
+}
+</script>
