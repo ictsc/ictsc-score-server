e4e08eab9a32be9227724083afe4b877ccd60bfa
uplus <uplus.e10@gmail.com>
2019-08-25 03:04:08 +0900

    fix answer page

--- ui/pages/answers.vue
+++ ui/pages/answers.vue
@@ -1,33 +1,53 @@
 <template>
   <v-container>
     <!-- TODO: 仮実装 -->
-    <v-layout column>
-      <v-flex>
-        <v-layout column align-center>
-          <page-title title="解答一覧" />
-        </v-layout>
-      </v-flex>
+    <v-layout column align-center>
+      <page-title title="解答一覧" />
 
-      <v-flex v-for="problem in problems" :key="problem.id">
-        <v-layout column>
-          <v-flex>
-            <h2>{{ problem.body.title }}</h2>
-          </v-flex>
+      <v-switch
+        v-model="showAll"
+        :label="showAll ? '全表示' : '未採点のみ'"
+        hide-details
+      />
 
-          <v-container fluid grid-list-md>
-            <v-layout wrap>
-              <v-flex v-for="answer in problem.answers" :key="answer.id" shrink>
-                <answer-list-card :answer="answer" />
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-layout>
-      </v-flex>
+      <v-select
+        v-model="problemCode"
+        :items="problems"
+        clearable
+        hide-details
+        item-text="title"
+        item-value="code"
+        label="問題名"
+        class="pb-3"
+      />
     </v-layout>
+
+    <!-- 一覧 -->
+    <v-container>
+      <v-layout v-for="problem in problems" :key="problem.id">
+        <div v-show="isDisplayProblem(problem)" class="mb-4">
+          <div class="title">{{ problem.code }} {{ problem.body.title }}</div>
+
+          <v-row class="mx-0" justify="start" align="start">
+            <template v-for="answer in filter(problem.answers)">
+              <answer-list-card
+                v-show="isDisplayAnswer(answer)"
+                :key="answer.id"
+                :answer="answer"
+                class="ma-1"
+              />
+            </template>
+
+            <v-spacer />
+          </v-row>
+        </div>
+      </v-layout>
+    </v-container>
   </v-container>
 </template>
 <script>
 import orm from '~/orm'
+import { JsonStroage } from '~/plugins/json-storage'
 import PageTitle from '~/components/atoms/PageTitle'
 import AnswerListCard from '~/components/molecules/AnswerListCard'
 
@@ -39,10 +59,13 @@ export default {
     AnswerListCard,
     PageTitle
   },
+  mixins: [
+    // 透過的にローカルストレージにアクセスできる
+    JsonStroage.accessor('answer-list', 'showAll', true),
+    JsonStroage.accessor('answer-list', 'problemCode', undefined)
+  ],
   computed: {
     problems() {
-      // TODO: bodyが無ければ loading
-      // TODO: エラー通知&表示
       const problems = orm.Problem.query()
         .with(['body', 'answers.team', 'answers.score', 'answers.problem.body'])
         .all()
@@ -51,7 +74,40 @@ export default {
     }
   },
   fetch() {
-    orm.Problem.eagerFetch({}, ['answers'])
+    orm.Problem.eagerFetch({}, ['answers', 'team'])
+    orm.Team.eagerFetch({}, [])
+  },
+  methods: {
+    // 各チームの最終解答のみの配列にする
+    shrinkAnswers(answers) {
+      const teamsAnswers = this.$_.groupBy(answers, answer => answer.teamId)
+
+      // そのチームの複数解答を最も新しい解答1つに上書き
+      return Object.keys(teamsAnswers).map(teamId =>
+        this.findLatestAnswer(teamsAnswers[teamId])
+      )
+    },
+    filterAnswers(answers) {
+      const result = this.shrinkAnswers(answers)
+
+      return result
+    },
+    sortByTeamNumber(answers) {
+      return this.$_.sortBy(answers, a => a.team.number)
+    },
+    filter(answers) {
+      return this.sortByTeamNumber(this.filterAnswers(answers))
+    },
+    isDisplayAnswer(answer) {
+      return this.showAll || !answer.hasPoint
+    },
+    isDisplayProblem(problem) {
+      if (!this.problemCode) {
+        return true
+      }
+
+      return problem.code === this.problemCode
+    }
   }
 }
 </script>
