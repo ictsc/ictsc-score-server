e0bde12bc2b409f9985b0d6e3a5fe10dc4f8f9a1
uplus <uplus.e10@gmail.com>
2019-09-26 01:36:28 +0900

    cleanup root query

--- api/app/graphql/types/query_type.rb
+++ api/app/graphql/types/query_type.rb
@@ -2,20 +2,22 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :me, Types::TeamType, null: true
-    field :contest_info, Types::ContestInfoType, null: false
-    field :categories, [Types::CategoryType], null: false
+    field :me,                   Types::TeamType,                 null: true
+    field :contest_info,         Types::ContestInfoType,          null: false
+    field :categories,           [Types::CategoryType],           null: false
+    field :problems,             [Types::ProblemType],            null: false
+    field :problem_environments, [Types::ProblemEnvironmentType], null: false
+    field :teams,                [Types::TeamType],               null: false
+    field :notices,              [Types::NoticeType],             null: false
+    field :sessions,             [Types::SessionType],            null: false
+
     field :problem, Types::ProblemType, null: true do
       argument :id, ID, required: true
     end
-    field :problems, [Types::ProblemType], null: false
-    field :problem_environments, [Types::ProblemEnvironmentType], null: false
+
     field :team, Types::TeamType, null: true do
       argument :id, ID, required: true
     end
-    field :teams, [Types::TeamType], null: false
-    field :notices, [Types::NoticeType], null: false
-    field :sessions, [Types::SessionType], null: false
 
     def me
       self.current_team!.readable(team: self.current_team!)
