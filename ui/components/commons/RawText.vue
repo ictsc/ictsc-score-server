b57cfbb81835b415be2998e62896e6bba56e9cb6
uplus <uplus.e10@gmail.com>
2019-09-29 21:17:55 +0900

    add button on the AnswerCard to toggle between Markdown and Raw

---
+++ ui/components/commons/RawText.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-textarea
+    :value="content"
+    auto-grow
+    solo
+    flat
+    hide-details
+    readonly
+    class="pa-0 ma-0 shrink-side-slot shrink-top"
+  />
+</template>
+<script>
+export default {
+  name: 'RawText',
+  props: {
+    content: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped lang="sass">
+// v-textareaの余分な余白を消す
+.shrink-side-slot
+  ::v-deep
+    div
+      .v-input__slot
+        padding: 0 0 !important
+
+.shrink-top
+  ::v-deep
+    textarea
+      padding-top: 0 !important
+      margin-top: 0 !important
+</style>
