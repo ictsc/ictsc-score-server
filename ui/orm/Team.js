ddd260177247fb95d8a7edef445093bd12b13a91
potyama <kamikaze117117@gmail.com>
2019-10-20 19:38:49 +0900

    レビュー箇所修正

--- ui/orm/Team.js
+++ ui/orm/Team.js
@@ -17,18 +17,23 @@ export default class Team extends BaseModel {
   get displayName() {
     return `No.${this.number} ${this.name}`
   }
+
   get numberStr() {
     return this.number.toString()
   }
+
   get isStaff() {
     return this.role === 'staff'
   }
+
   get isAudience() {
     return this.role === 'audience'
   }
+
   get isPlayer() {
     return this.role === 'player'
   }
+
   static get players() {
     return Team.all().filter(t => t.isPlayer)
   }
