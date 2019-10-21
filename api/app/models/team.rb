7b28649c77d578bb8fac08b5cba15bba350a2939
uplus <uplus.e10@gmail.com>
2019-10-21 16:26:10 +0900

    fix Team#notices association

--- api/app/models/team.rb
+++ api/app/models/team.rb
@@ -14,7 +14,7 @@ class Team < ApplicationRecord
   has_many :attachments,           dependent: :nullify
   has_many :first_correct_answers, dependent: :destroy
   has_many :issues,                dependent: :destroy
-  has_many :notices,               dependent: :nullify, inverse_of: 'target_team'
+  has_many :notices,               dependent: :nullify, inverse_of: 'target_team', foreign_key: 'target_team_id'
   has_many :problem_environments,  dependent: :destroy
 
   # 値が大きいほど大体権限が高い
