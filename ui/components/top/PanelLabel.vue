5dd29fae290ab1500d84e80ad4c71309dad9c0e4
uplus <uplus.e10@gmail.com>
2020-01-12 07:43:22 +0900

    add PanelLabel component

---
+++ ui/components/top/PanelLabel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-row justify="center">
+      <div class="headline">{{ label }}</div>
+      <slot />
+    </v-row>
+
+    <v-divider class="mt-2 mb-4" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'PanelLabel',
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
