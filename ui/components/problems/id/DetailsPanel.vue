4321f7a767380b172a554eb6a8951f5c2719c42c
uplus <uplus.e10@gmail.com>
2020-02-13 18:46:23 +0900

    reorder problem DetailsPanel

--- ui/components/problems/id/DetailsPanel.vue
+++ ui/components/problems/id/DetailsPanel.vue
@@ -22,13 +22,6 @@
     <!-- 問題情報 -->
     <info-chips-area :problem="problem" class="mt-2 ml-0" />
 
-    <!-- 運営メモ -->
-    <v-sheet v-if="!!problem.secretText" class="pa-2 elevation-2">
-      <span class="pa-2 body-2">運営用メモ</span>
-      <v-divider class="pb-1" />
-      <markdown :content="problem.secretText" />
-    </v-sheet>
-
     <!-- 補足 -->
     <supplement-area
       v-if="problem.supplements.length !== 0 || isStaff"
@@ -36,6 +29,13 @@
       :problem-code="problem.code"
       class="pt-2"
     />
+
+    <!-- 運営メモ -->
+    <v-sheet v-if="!!problem.secretText" class="pa-2 elevation-2">
+      <span class="pa-2 body-2">運営用メモ</span>
+      <v-divider class="pb-1" />
+      <markdown :content="problem.secretText" />
+    </v-sheet>
 
     <!-- 環境 -->
     <environment-area
