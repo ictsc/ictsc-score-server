0cbc495a5b8e6b65c7ad498d7fa4409de175d068
uplus <uplus.e10@gmail.com>
2020-03-21 03:20:52 +0900

    reimplement ExportScoresButton

--- ui/components/settings/ExportScoresButton.vue
+++ ui/components/settings/ExportScoresButton.vue
@@ -5,7 +5,7 @@
     </v-row>
 
     <v-row justify="start">
-      <v-btn :loading="loading" small @click="exportData">
+      <v-btn :loading="loading" small @click="click">
         エクスポート
         <v-icon>mdi-file-export</v-icon>
       </v-btn>
@@ -19,98 +19,27 @@ export default {
   name: 'ExportImportButtons',
   data() {
     return {
-      loading: false,
-      problems: [],
-      teams: []
+      loading: false
     }
   },
   methods: {
-    // TODO: 廃止予定
-    findEffectAnswer(answers) {
-      // TODO: 予選はとりあえずこれ
-      return this.findNewer(answers)
+    async getReportCards() {
+      await orm.Queries.reportCards()
+
+      const list = orm.ReportCard.all()
+      list.forEach(report => delete report.$id)
+      return list
     },
-
-    // TODO: API側に持ってくる
-    async fetch() {
-      await orm.Queries.problemsAnswers()
-      await orm.Queries.teams()
-
-      this.problems = this.sortByOrder(
-        orm.Problem.query()
-          .with(['body', 'answers.team'])
-          .all()
-      )
-
-      this.teams = this.sortByNumber(orm.Team.playersWithoutTeam99)
-    },
-    async exportData() {
+    async click() {
       this.loading = true
-      await this.fetch()
-
-      let ungradedCount = 0
-
-      // 各問題の解答を1チーム1つにする(得点)
-      // problems[0].answers[teamId] == 最終提出解答得点
-      // { '問題名' : [{ team: 'Team名', point: 999 }, ...], ... }
-      this.problems.forEach(problem => {
-        // この問題の解答をチーム毎に分ける
-        // { teamId: [ans1, ans2, ...], ... }
-        const grouped = this.$_.groupBy(problem.answers, a => a.teamId)
-
-        problem.answers = Object.keys(grouped).reduce((answers, teamId) => {
-          const latestAnswer = this.findEffectAnswer(grouped[teamId])
-
-          // 未採点ならマズイので警告
-          if (!latestAnswer.hasScore) {
-            ungradedCount += 1
-            const team = orm.Team.find(teamId)
-            console.warn(
-              `未採点 問題: ${problem.title}, チーム: ${team.displayName}`
-            )
-          }
-
-          answers[teamId] = latestAnswer.point
-          return answers
-        }, {})
-      })
-
-      if (ungradedCount !== 0) {
-        this.notifyWarning({
-          message: `有効解答の内 ${ungradedCount}件が未採点です`
-        })
-      }
-
-      // 未提出,未採点なら0ではなくnullになる
-      const data = this.problems.reduce((obj, problem) => {
-        // 解答をチームでイテレーションする
-        obj[problem.title] = this.teams.map(team =>
-          this.buildField(team, problem)
-        )
-
-        obj[problem.title].unshift({
-          team: '満点',
-          point: problem.perfectPoint
-        })
-
-        return obj
-      }, {})
 
       this.download(
         'text/json',
         `scores ${this.currentDateTimeString()}.json`,
-        JSON.stringify(data)
+        JSON.stringify(await this.getReportCards())
       )
-      this.loading = false
-    },
-    buildField(team, problem) {
-      const point = problem.answers[team.id]
 
-      return {
-        team: team.name,
-        // 解答がないならundefinedになる
-        point: point === undefined ? null : point
-      }
+      this.loading = false
     }
   }
 }
