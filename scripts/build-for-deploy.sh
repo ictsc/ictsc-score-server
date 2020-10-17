085a794cce03bdbe522f46e8cc4fc87514ef8108
uplus <uplus.e10@gmail.com>
2020-10-18 02:55:26 +0900

    Add build and push script

---
+++ scripts/build-for-deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash -ue
+
+iss_tag() {
+  prefix='upluse10/ictsc-score-server'
+  service="${1:? api or ui}"
+  echo
+
+  git log -1 --oneline
+  commit="$(git log -1 --oneline | awk '$0=$1')"
+  echo
+
+  docker image ls | grep "${prefix}" | grep "\s${service}\s"
+  src_image_hash="$(docker image ls | grep "${prefix}" | grep "\s${service}\s" | awk '$0=$3')"
+  echo
+
+  echo "service:        ${service}"
+  echo "commit hash:    ${commit}"
+  echo "src image hash: ${src_image_hash}"
+
+  echo
+  image_name="${prefix}:${service}-${commit}"
+  docker tag "${src_image_hash}" "${image_name}"
+
+  echo "docker push ${image_name}"
+  docker push "${image_name}"
+  echo "${image_name}"
+}
+
+docker-compose build api ui
+
+iss_tag api
+iss_tag ui
