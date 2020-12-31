d536d0b8c86a67e96680f70a49a04ab5f0a0d5b9
uplus <uplus.e10@gmail.com>
2020-12-31 17:26:18 +0900

    Fix comments

--- ui/plugins/moment-update-locale.js
+++ ui/plugins/moment-update-locale.js
@@ -1,5 +1,5 @@
 // デフォルトだとmomentに無効な値を与えると"Invalid Date"が返る
-// 扱いづらいので変わりにnullを返す。 つもりが何故かundefinedになる
+// 扱いづらいので代わりにnullを返す。 つもりが何故かundefinedになる
 
 export default (ctx) => {
   ctx.$moment.updateLocale(ctx.$moment.locale(), { invalidDate: null })
