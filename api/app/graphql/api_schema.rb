87b0cf771a3abddefc61dc0e7e7ed210fd7dabc4
uplus <uplus.e10@gmail.com>
2021-01-02 00:35:59 +0900

    Replace new graphql runtime module

--- api/app/graphql/api_schema.rb
+++ api/app/graphql/api_schema.rb
@@ -5,6 +5,10 @@ class ApiSchema < GraphQL::Schema
   query(Types::QueryType)
   use GraphQL::Batch
 
+  # Opt in to the new runtime (default in future graphql-ruby versions)
+  use GraphQL::Execution::Interpreter
+  use GraphQL::Analysis::AST
+
   # Add built-in connections for pagination
   # use GraphQL::Pagination::Connections
 
