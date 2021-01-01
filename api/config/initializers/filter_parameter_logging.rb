e3f876a9cfd546655fa0456d62694cfa4d60db8c
uplus <uplus.e10@gmail.com>
2021-01-01 19:31:11 +0900

    Execute rails app:update

--- api/config/initializers/filter_parameter_logging.rb
+++ api/config/initializers/filter_parameter_logging.rb
@@ -3,4 +3,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[password channel secret_text secretText]
+Rails.application.config.filter_parameters += %i[
+  passw secret token _key crypt salt certificate otp ssn
+  passward channel secret_text secretText
+]
--- api/config/puma.rb
+++ api/config/puma.rb
@@ -10,9 +10,14 @@ max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
 
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+#
+worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch('PORT', 3000)
+port ENV.fetch('PORT', 3000)
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -34,7 +39,8 @@ workers ENV.fetch('WEB_CONCURRENCY', 2)
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-preload_app! if Rails.env.production?
+# preload_app!
+# worker数が1以上だと自動で有効になる
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
