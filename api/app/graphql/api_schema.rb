8b4736cfe36dd35d25c30c73bf5002a324aad443
uplus <uplus.e10@gmail.com>
2021-01-02 00:07:45 +0900

    Remove comment

--- api/app/graphql/api_schema.rb
+++ api/app/graphql/api_schema.rb
@@ -6,8 +6,6 @@ class ApiSchema < GraphQL::Schema
   context_class CustomContext
   use GraphQL::Batch
 
-  # TODO: mutationとqueryが同時に来たら事故る気がする -> できない気がする
-  # TODO: https://graphql-ruby.org/schema/definition.html#execution-configuration
   # max_depth 10
   # max_complexity 300
   # default_max_page_size 20
