c321bb807953647611e366c7381cba647ae81308
uplus <uplus.e10@gmail.com>
2021-01-02 23:34:47 +0900

    Cleanup base_mutation

--- api/app/graphql/mutations/base_mutation.rb
+++ api/app/graphql/mutations/base_mutation.rb
@@ -10,26 +10,30 @@ module Mutations
     # This is used for generating the `input: { ... }` object type
     input_object_class Types::BaseInputObject
 
-    # あまりにも多用するのでショートハンド化
-    def current_team!
-      self.context.current_team!
-    end
-
     # Mutationのインスタンスから名前にアクセスするショートハンド
     def graphql_name
       self.class.graphql_name
     end
 
+    private
+
+    # 多用するのでショートハンド化
+    def current_team!
+      self.context.current_team!
+    end
+
+    # エラーメッセージをgrpahql-rubyが処理できる形式で登録する
     def add_error_message(message, ast_node: nil, options: nil, extensions: nil)
       Rails.logger.error message
       self.context.add_error(GraphQL::ExecutionError.new(message, ast_node: ast_node, options: options, extensions: extensions))
     end
 
+    # ActiveRecordを受け取って、Errorsを親切なメッセージに変換してadd_error_messageに渡す
     def add_errors(*records)
       records.each do |record|
         record.errors.each do |error|
           message = "#{record.model_name} #{error.attribute} #{error.message}"
-          self.add_error_message(GraphQL::ExecutionError.new(message))
+          self.add_error_message(message)
         end
       end
 
